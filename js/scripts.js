bbecdda4759e8437589ae107f9bb7141c9c93649
Victor962-hub <vn20012010@gmail.com>
2026-05-06 17:09:36 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
 // ATIVIDADE 01
 const ligar = () => {
+  let lampada = document.getElementById("lampada");
 
+  if (lampada.src.includes("apagada.jpg")) {
+    lampada.src = "img/acesa.jpg";
+  } else {
+    lampada.src = "img/apagada.jpg";
+  }
 };
 // -----------------------------------------
 
 // ATIVIDADE 02
 const gerarImagem = () => {
+    let qtdImagem = Number(document.getElementById("inQtdImg").value);
+    let canvas = document.getElementById("canvas");
+    canvas.innerHTML="";
+    
+    if(qtdImagem <= 0){
+        alert("O campo quantidade não pode estar nulo ou negativo");
 
+    }else{ 
+        for (let i = 1; i <= qtdImagem; i++) {
+            canvas.innerHTML += `<img id="logo" src="img/logo.png"
+            alt="Logotipo SENAI" class="imagens" />`;
+        }
+    }
 };
 // -----------------------------------------
 
 //ATIVIDADE 03
-const validaForm = () => {
-  
-};
+const validaForm = () => {};
 // -----------------------------------------
 
 // ATIVIDADE 04
-const calcular = () => {
- 
-};
+const calcular = () => {};
